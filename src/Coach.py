2effcdd59091d5b7a823464292fcb5ecfc241969
Andrew Casner <a.casner@comcast.net>
2018-10-31 21:03:07 -0600

    Added Coach Class

---
+++ src/Coach.py
@@ -0,0 +1,26 @@
+import Player.py
+
+class Coach(Player):
+
+    def __init__(self):
+        self.wins = 0
+        self.losses = 0
+
+    def getWins(self):
+        return self.wins
+
+    def addWin(self):
+        self.wins = self.wins + 1
+
+    def getLosses(self):
+        return self.losses
+
+    def addLoss(self):
+        self.losses = self.losses + 1
+
+    def getRecord(self):
+        return self.wins/self.losses
+
+    def setRecord(self, w, l):
+        self.wins = w
+        self.losses = l
