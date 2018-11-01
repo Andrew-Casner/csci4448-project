6fa7408fb0154609baa41d8c1e0fd52429c29a92
Andrew Casner <a.casner@comcast.net>
2018-10-31 20:55:41 -0600

    Added Player Class

---
+++ src/Player.py
@@ -0,0 +1,23 @@
+class Player:
+
+    def __init__(self, id):
+        self.id = id
+        self.firstName = ""
+        self.lastName = ""
+
+    def getId(self):
+        return self.id
+
+    def getFirstName(self):
+        return self.firstName
+
+    def getLastName(self):
+        return self.lastName
+
+    def getFullName(self):
+        return self.firstName + " " + self.lastName
+
+    def editName(self, first, last):
+        self.firstName = first
+        self.lastName = last
+
